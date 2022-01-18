27f8865a80ca3cda05912830b3a2aa541bb0df67
Vilmokas <42962270+Vilmokas@users.noreply.github.com>
2022-01-18 14:56:26 +0200

    feat: change style of hex box, current color box and ignore current color marker colors

--- ColorPicker.js
+++ ColorPicker.js
@@ -36,15 +36,24 @@ const sliceColors = [
 const sliceSize = 360 / sliceCount;
 const angleSize = (Math.PI * 2) / sliceCount;
 
-let colorPos = { x: 0, y: 0 };
-let colorCurrent = "rgb(0, 0, 0)";
+let mainColorPos = { x: getX(0), y: getY(0) };
+let secondColorPos = { x: center.x, y: center.y };
 let rgba = [0, 0, 0];
+let mainColor = [0, 0, 0];
 
-function initColorPicker() {
-  const path = c.getContext("2d");
+const path = c.getContext("2d");
 
-  path.fillStyle = colorCurrent;
-  path.fillRect(0, 0, 50, 25);
+const drawColorWheel = () => {
+  let context = c.getContext("2d");
+  context.clearRect(0, 0, c.width, c.height);
+
+  path.lineWidth = wheelWidth + 4;
+
+  // Draw corcle border
+  path.beginPath();
+  path.arc(center.x, center.y, wheelRadius, 0, 2 * Math.PI);
+  path.strokeStyle = "#454545";
+  path.stroke();
 
   path.lineWidth = wheelWidth;
 
@@ -69,6 +78,14 @@ function initColorPicker() {
     path.stroke();
   }
   let ctx = c.getContext("2d");
+
+  // Draw triangle border
+  ctx.beginPath();
+  ctx.moveTo(getX(0) - 14, getY(0));
+  ctx.lineTo(getX(120) + 7, getY(120) - 7);
+  ctx.lineTo(getX(240) + 7, getY(240) + 7);
+  ctx.fillStyle = "#454545";
+  ctx.fill();
 
   // Draw triangle white and black color
   ctx.beginPath();
@@ -97,30 +114,91 @@ function initColorPicker() {
     getX(0) - 140,
     getY(0)
   );
-  grdBase.addColorStop(0, `rgba(${rgba[0]},${rgba[1]},${rgba[2]},1)`);
-  grdBase.addColorStop(1, `rgba(${rgba[0]},${rgba[1]},${rgba[2]},0)`);
+  grdBase.addColorStop(
+    0,
+    `rgba(${mainColor[0]},${mainColor[1]},${mainColor[2]},1)`
+  );
+  grdBase.addColorStop(
+    1,
+    `rgba(${mainColor[0]},${mainColor[1]},${mainColor[2]},0)`
+  );
   ctx.fillStyle = grdBase;
   ctx.fill();
+};
+
+function initColorPicker() {
+  drawColorWheel();
 
   c.onclick = function (e) {
+    drawColorWheel();
+    let colorPos = { x: 0, y: 0 };
     colorPos.x = (e.offsetX / c.clientWidth) * c.width;
     colorPos.y = (e.offsetY / c.clientHeight) * c.height;
 
-    let imgData = path.getImageData(colorPos.x, colorPos.y, 1, 1);
-    rgba = imgData.data;
-    let color = "rgb(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ")";
-    colorCurrent = color;
-    console.log("%c" + color, "color:" + color);
+    // Change the main color (outer ring)
+    if (isInsideRing(colorPos.x, colorPos.y, wheelRadius, wheelWidth)) {
+      mainColorPos.x = colorPos.x;
+      mainColorPos.y = colorPos.y;
+      let imgData = path.getImageData(
+        mainColorPos.x,
+        mainColorPos.y,
+        1,
+        1
+      ).data;
+      mainColor = [imgData[0], imgData[1], imgData[2]];
+    }
 
+    // Change the 2nd color (triangle)
+    if (
+      isInsideTriangle(
+        colorPos,
+        { x: getX(0) - 18, y: getY(0) },
+        { x: getX(120) + 9, y: getY(120) - 9 },
+        { x: getX(240) + 9, y: getY(240) + 9 }
+      )
+    ) {
+      secondColorPos.x = colorPos.x;
+      secondColorPos.y = colorPos.y;
+    }
+
+    drawColorWheel();
+
+    console.log(`x: ${secondColorPos.x}, y: ${secondColorPos.y}`);
+    let imgData = path.getImageData(secondColorPos.x, secondColorPos.y, 1, 1);
+    rgba = imgData.data;
+
+    console.log(rgbToHex(rgba[0], rgba[1], rgba[2]));
     currentColor.value = rgbToHex(rgba[0], rgba[1], rgba[2]);
     currentColor.dispatchEvent(new Event("change"));
-    let context = c.getContext("2d");
-    context.clearRect(0, 0, c.width, c.height);
 
-    initColorPicker();
+    // Draw markers at the end to avoid detecting their color when picking colors
     drawColorMarker();
   };
 }
+
+const isInsideRing = (x, y, r, w) => {
+  return (
+    (x - center.x) * (x - center.x) + (y - center.y) * (y - center.y) >
+      (r - w / 2) * (r - w / 2) &&
+    (x - center.x) * (x - center.x) + (y - center.y) * (y - center.y) <
+      (r + w / 2) * (r + w / 2)
+  );
+};
+
+const isInsideTriangle = (p, p0, p1, p2) => {
+  var A =
+    (1 / 2) *
+    (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
+  var sign = A < 0 ? -1 : 1;
+  var s =
+    (p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * p.x + (p0.x - p2.x) * p.y) *
+    sign;
+  var t =
+    (p0.x * p1.y - p0.y * p1.x + (p0.y - p1.y) * p.x + (p1.x - p0.x) * p.y) *
+    sign;
+
+  return s > 0 && t > 0 && s + t < 2 * A * sign;
+};
 
 function componentToHex(c) {
   var hex = c.toString(16);
@@ -133,11 +211,17 @@ function rgbToHex(r, g, b) {
 }
 
 const drawColorMarker = () => {
+  // Draw circle marker
   let ctx = c.getContext("2d");
   ctx.beginPath();
-  ctx.arc(colorPos.x, colorPos.y, 7, 0, 2 * Math.PI);
+  ctx.arc(mainColorPos.x, mainColorPos.y, 7, 0, 2 * Math.PI);
   ctx.strokeStyle = "black";
   ctx.lineWidth = 2;
+  ctx.stroke();
+
+  //Draw triangle marker
+  ctx.beginPath();
+  ctx.arc(secondColorPos.x, secondColorPos.y, 7, 0, 2 * Math.PI);
   ctx.stroke();
 };
 
